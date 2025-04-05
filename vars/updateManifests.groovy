4494be7510150efbdb3aa25f39aafefd7cb876f0
RanaSalem2412 <ranasalem923@gmail.com>
2025-04-05 19:25:04 +0200

    Update updateManifests.groovy

--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -1,8 +1,11 @@
 def call() {
     echo 'Updating Kubernetes manifests with the new Docker image...'
 
-    // قراءة محتوى deployment.yaml و استبدال الـ image بالـ Docker image الجديد
-    sh """
-        sed -i 's|image: .*|image: ${DOCKER_IMAGE}:${DOCKER_TAG}|' k8s-manifests/deployment.yaml
-    """
+    // الانتقال إلى مجلد الكود الذي يحتوي على الملفات
+    dir('jenkins-shared-library/k8s-manifests') {
+        // قراءة محتوى deployment.yaml و استبدال الـ image بالـ Docker image الجديد
+        sh """
+            sed -i 's|image: .*|image: ${DOCKER_IMAGE}:${DOCKER_TAG}|' deployment.yaml
+        """
+    }
 }
