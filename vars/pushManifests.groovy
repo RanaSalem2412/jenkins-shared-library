f589430a83557340cecad8108dca98649aad006a
RanaSalem2412 <ranasalem923@gmail.com>
2025-04-05 12:41:04 +0200

    Update pushManifests.groovy

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -1,1 +1,7 @@
+def call() {
+    echo 'Pushing updated Kubernetes manifests...'
+    sh 'git add k8s-manifests/'
+    sh 'git commit -m "Updated image tag in deployment.yaml"'
+    sh 'git push'
+}
 
