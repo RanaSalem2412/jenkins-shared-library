d30ab7f7a15f64110f44f496228c93df2fa7bc8e
RanaSalem2412 <ranasalem923@gmail.com>
2025-04-05 14:52:30 +0200

    Update updateManifests.groovy

--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -1,5 +1,9 @@
 def call() {
-    echo 'Updating Kubernetes manifests...'
-    sh 'sed -i "s/old-tag/${env.GIT_COMMIT}/g" k8s-manifests/deployment.yaml'
+    echo 'Updating Kubernetes manifests with the new Docker image...'
+
+    // قراءة محتوى deployment.yaml و استبدال الـ image بالـ Docker image الجديد
+    sh """
+        sed -i 's|image: .*|image: ${DOCKER_IMAGE}:${DOCKER_TAG}|' k8s/deployment.yaml
+    """
 }
 
