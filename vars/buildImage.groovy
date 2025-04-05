6926eee35dd89f16d3d1c17b24e211ea8e9cda89
RanaSalem2412 <ranasalem923@gmail.com>
2025-04-05 14:53:22 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,4 @@
 def call() {
     echo 'Building Docker image...'
-    sh 'docker build -t my-repo/my-app .'
+    sh 'docker build -t ${DOCKER_IMAGE}:${DOCKER_TAG} .'  // قمنا بإضافة مسار العمل في الـ Jenkinsfile مباشرة
 }
-
