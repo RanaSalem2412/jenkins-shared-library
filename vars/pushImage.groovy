3faedc3eabed9e876d7cbcc7037ae5f0127f7a10
RanaSalem2412 <ranasalem923@gmail.com>
2025-04-05 17:49:01 +0200

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,4 +1,6 @@
 def call() {
-    echo 'Pushing Docker image to Docker Hub...'
+    echo "Checking if Docker image exists..."
+    sh "docker images | grep ${DOCKER_IMAGE}:${DOCKER_TAG} || exit 1"
+    echo "Pushing Docker image ${DOCKER_IMAGE}:${DOCKER_TAG} to Docker Hub..."
     sh 'docker push ${DOCKER_IMAGE}:${DOCKER_TAG}'
 }
