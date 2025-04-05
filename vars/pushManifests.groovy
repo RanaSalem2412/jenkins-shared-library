f13efc84d2f50dae03957439a429dcf173e71a11
RanaSalem2412 <ranasalem923@gmail.com>
2025-04-05 20:25:26 +0200

    Update pushManifests.groovy

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -3,11 +3,15 @@ def call() {
 
     // الانتقال إلى مجلد الكود الذي يحتوي على الملفات
     dir('jenkins-shared-library/k8s-manifests') {
-        // دفع التغييرات إلى الـ Git repository
-        sh """
-            git add deployment.yaml service.yaml
-            git commit -m "Update Kubernetes manifests with new Docker image"
-            git push origin main
-        """
+        // دفع التغييرات إلى الـ Git repository باستخدام بيانات الاعتماد
+        withCredentials([usernamePassword(credentialsId: 'git_credential', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
+            sh """
+                git config --global user.email "your-email@example.com"
+                git config --global user.name "Your Name"
+                git add deployment.yaml service.yaml
+                git commit -m "Update Kubernetes manifests with new Docker image"
+                git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/RanaSalem2412/jenkins-shared-library.git HEAD:main
+            """
+        }
     }
 }
